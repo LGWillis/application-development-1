c6fe1ec5a2c4a93f76b262c26be44b7fc31228ae
Luke <lukewillis196@icloud.com>
2026-04-20 19:59:35 -0400

    Require routes in server.db

--- final-project/server.js
+++ final-project/server.js
@@ -1,33 +1,33 @@
+
+require("dotenv").config();
+require("./db");
+
 const express = require("express");
 const session = require("express-session");
-require("dotenv").config();
+
+
+const authRoutes = require("./routes/authRoutes");
+const recipeRoutes = require("./routes/recipeRoutes");
 
 const app = express();
 
 app.use(express.json());
 
+
+
 app.use(session({
-    secret: "secret-key",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
 }));
+
+
+app.use("/auth", authRoutes);
+app.use("/recipes", recipeRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: "API is running" });
 });
 
-app.get("/test-db", (req, res) => {
-    const db = require("./db");
 
-    db.query("SELECT 1", (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.json({ message: "Database working", results });
-    });
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(3000, () => console.log("Server running on port 3000"));
